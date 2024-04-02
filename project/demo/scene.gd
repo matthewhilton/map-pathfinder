405797b185ddc37e18674daf7d4e9da923680824
Matthew Hilton <=>
2024-04-02 23:24:30 +1000

    Working skeletonisation library, but doesn't seem to be passing image correctly yet

--- project/demo/scene.gd
+++ project/demo/scene.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var test_image: Image
+
 func _ready() -> void:
 	print("Hello GDScript!")
 	$MyNode.hello_node()
@@ -7,4 +9,6 @@ func _ready() -> void:
 	
 	var map = BitMap.new()
 	map.create(Vector2i(1024,1024))
+	map.create_from_image_alpha(test_image)
+	map.convert_to_image().save_png("res://bitmap.png")
 	MapUtils.process_map(map)
--- src/map_utils.cpp
+++ src/map_utils.cpp
@@ -2,8 +2,10 @@
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/bit_map.hpp>
+#include <godot_cpp/classes/image.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <godot_cpp/classes/ref.hpp>
+#include "trace_skeleton.cpp"
 
 using namespace godot;
 
@@ -21,5 +23,37 @@ MapUtils::~MapUtils()
 }
 
 void MapUtils::process_map(Ref<BitMap> map) {
-    UtilityFunctions::print("TODO process map");
+    UtilityFunctions::print("Processing map");
+
+    // Convert image into byte buffer that can be read by opencv.
+    Ref<Image> temp_image = map->convert_to_image();
+    PackedByteArray buffer = temp_image->get_data();
+
+    skeleton_tracer_t* T = new skeleton_tracer_t();
+    T->W = 1024;
+    T->H = 1024;
+
+    T->im = (unsigned char*)malloc(sizeof(unsigned char)*T->W*T->H);
+    memcpy(T->im, buffer.ptrw(), buffer.size());
+
+    skeleton_tracer_t::polyline_t* p =  (skeleton_tracer_t::polyline_t*)T->trace_skeleton(0, 0, T->W, T->H, 0);
+
+    // https://github.com/LingDong-/skeleton-tracing/blob/master/cpp/example2.cpp
+    int count = 0;
+    skeleton_tracer_t::polyline_t* it = p; //iterator
+    while(it){
+        skeleton_tracer_t::point_t* jt = it->head;
+        while(jt){
+            UtilityFunctions::print(jt->x, " ", jt->y);
+         count++;
+        jt = jt->next;
+        }
+        it = it->next;
+    }
+
+    unsigned char first = buffer.ptr()[0];
+    unsigned char last = buffer.ptr()[1024*1024];
+    UtilityFunctions::print("polyline count: ", count, " first ", first, " last ", last);
+    
+    // TODO cleanup.
 }
